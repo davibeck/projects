4cd49b6358b7be0e53694b03fc93a9c9d585e57a
Davi Becker <davi.becker@hotmail.com>
2022-06-24 13:52:52 -0300

    adding dbsmoney as projeto2

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { GlobalStyles } from './styles/global';
+import { Header } from './components/Header';
+import Modal from 'react-modal'
+import { Dashboard } from './components/Dashboard';
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './hooks/useTransactions';
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const[isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal(){
+      setIsNewTransactionModalOpen(true)
+  }
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModalOpen(false)
+  }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+        <Dashboard />
+        <NewTransactionModal
+          isOpen ={isNewTransactionModalOpen}
+          onRequestClose = {handleCloseNewTransactionModal} />
+        <GlobalStyles />
+    </TransactionsProvider>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createServer, Model } from 'miragejs';
+import { App } from './App';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance web',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new  Date('2021-06-14 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdrawn',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new  Date('2021-06-16 09:00:00')
+        }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+
+    this.post('transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  }
+})
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
