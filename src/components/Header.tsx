04470b888a80ed8bb73ed24fe5a10584e93d1250
Davi Becker <davi.becker@hotmail.com>
2023-02-14 20:49:35 -0300

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import { Flex, Input, Text } from "@chakra-ui/react";
+import { generateKey } from "crypto";
+
+export function Header() {
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      h="20"
+      maxWidth={1480}
+      mx="auto"
+      mt="4"
+      px="6"
+      align="center"
+    >
+      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+        dashgo
+        <Text as="span" marginLeft="1" color="pink.500">
+          .
+        </Text>
+      </Text>
+
+      <Flex
+        as="label"
+        flex="1"
+        py="4"
+        px="8"
+        ml="6"
+        maxWidth={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        bg='gray.800'
+        borderRadius='full'
+      >
+        <Input color='gray.500' variant='unstyled' px='4' mr='4' placeholder="Buscar na plataforma" _placeholder={{color: 'gray.400'}}>
+        </Input>
+      </Flex>
+    </Flex>
+  );
+}
