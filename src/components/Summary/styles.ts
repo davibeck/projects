bdf60da544f52dc78d2e425407297b4bd7094032
Davi Becker <davi.becker@hotmail.com>
2023-03-15 16:00:02 -0300

    new mobile style

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,34 +1,38 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 2rem;
-    margin-top: -10rem;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  margin-top: -10rem;
 
-    div {
-        background-color: var(--shape);
-        padding: 1.5rem 2rem;
-        border-radius: 0.25rem;
-        color: var(--text-title);
+  @media screen and (max-width: 900px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 
-        header {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        }
+  div {
+    background-color: var(--shape);
+    padding: 1.5rem 2rem;
+    border-radius: 0.25rem;
+    color: var(--text-title);
 
-        strong {
-        display: block;
-        margin-top: 1rem;
-        font-size: 2rem;
-        font-weight: 500;
-        line-height: 3rem;
-        }
-
-        &.highlight-background {
-            background-color: var(--green);
-            color: #fff;
-        }
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
     }
+
+    strong {
+      display: block;
+      margin-top: 1rem;
+      font-size: 2rem;
+      font-weight: 500;
+      line-height: 3rem;
+    }
+
+    &.highlight-background {
+      background-color: var(--green);
+      color: #fff;
+    }
+  }
 `;
